e0cdc2ca8257cb7a11b865b277d20d3841399057
LeonardoRCosta <rm94286@fiap.com.br>
2022-11-30 10:55:49 -0300

    Type basics class.

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,13 @@
-var Name = "Leo";
-console.log(Name);
+var character = "Leo";
+var age = 20;
+var isBlackBelt = false;
+// character = 20 -> Assignment error
+character = "Leonardo";
+// age = "Leo" -> Assignment error
+age = 18;
+// isBlackBelt = "yes" -> Assignment error
+isBlackBelt = true;
+// const circ = diameter => diameter * Math.PI;
+// console.log(circ("lol")) -> NaN
+var circ = function (diameter) { return diameter * Math.PI; };
+console.log(circ(10));
--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,18 @@
-const Name = "Leo";
-console.log(Name)
+let character = "Leo";
+let age = 20;
+let isBlackBelt = false;
+
+// character = 20 -> Assignment error
+character = "Leonardo";
+
+// age = "Leo" -> Assignment error
+age = 18;
+
+// isBlackBelt = "yes" -> Assignment error
+isBlackBelt = true;
+
+// const circ = diameter => diameter * Math.PI;
+// console.log(circ("lol")) -> NaN
+
+const circ = (diameter: number) => diameter * Math.PI;
+console.log(circ(10));
