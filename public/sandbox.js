c1aa1e6a009df6859eba0f109a23c3437becdb77
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-04 20:53:41 -0300

    Function types basic

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Hello");
+let greet;
+// greet = 'hello' -> Assignment error
+greet = () => {
+    console.log("hello");
+};
+//                                 ? means an optional argument
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+const minus = (a, b) => {
+    return a - b;
+};
+const result = minus(5, 10);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log("Hello")
+let greet: Function;
+
+// greet = 'hello' -> Assignment error
+
+greet = () => {
+  console.log("hello");
+};
+
+//                                 ? means an optional argument
+const add = (a: number, b: number, c?: number | string): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+}
+
+const result = minus(5, 10);
