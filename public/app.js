781d5ddc46dac136d8a0df4a64e6f25ca637ce43
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-07 10:44:07 -0300

    Interfaces with classes and DOM

--- public/app.js
+++ public/app.js
@@ -1,24 +1,6 @@
-const me = {
-    name: "Leo",
-    age: 18,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log(`I spent ${amount}`);
-        return amount;
-    },
-};
-const greetPerson = (person) => console.log(`greetings ${person.name}`);
-greetPerson(me);
-console.log(me);
-import { Invoice } from "./classes/Invoice.js"; // because we are using javascript in the browser we need to import the js module.
-const invOne = new Invoice("Leonardo", "Working on PeerBR's payment API", 1300);
-const invTwo = new Invoice("Rafael", "Working on PeerBR's payment API", 1500);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => console.log(inv.client, inv.amount, inv.format()));
+import { Payment } from "./classes/Payment.js";
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/listTemplate.js";
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
@@ -26,7 +8,16 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,7 @@
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-const me: IsPerson = {
-  name: "Leo",
-  age: 18,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log(`I spent ${amount}`);
-    return amount;
-  },
-};
-
-const greetPerson = (person: IsPerson) => console.log(`greetings ${person.name}`);
-
-greetPerson(me);
-
-console.log(me);
-
-import { Invoice } from "./classes/Invoice.js"; // because we are using javascript in the browser we need to import the js module.
-
-const invOne = new Invoice("Leonardo", "Working on PeerBR's payment API", 1300);
-const invTwo = new Invoice("Rafael", "Working on PeerBR's payment API", 1500);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach((inv) => console.log(inv.client, inv.amount, inv.format()));
+import { Payment } from "./classes/Payment.js";
+import { Invoice } from "./classes/Invoice.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./classes/listTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
@@ -43,8 +12,18 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, 'end');
 });
